73f2e0e6f03d590f71165733193e507f631811fd
tsilm08 <takanori.yoshinori@outlook.jp>
2021-03-09 16:24:52 +0900

    Character.h/cppに床、壁に使用するオブジェクトを追加

--- FullSample100/GameSources/Character.cpp
+++ FullSample100/GameSources/Character.cpp
@@ -4,10 +4,131 @@
 */
 
 #include "stdafx.h"
-#include "Project.h"
+#include "Character.h"
+//#include "Project.h"
 
 namespace basecross{
+	//-----------------------------------------------------------------
+	//  è∞
+	//-----------------------------------------------------------------
+	Floor::Floor(const shared_ptr<Stage>& StagePtr,
+		const Vec3& Scale,
+		const Vec3& Rotation,
+		const Vec3& Position,
+		float UPic,
+		float VPic,
+		const wstring& Texture
+	) :
+		GameObject(StagePtr),
+		m_Scale(Scale),
+		m_Rotation(Rotation),
+		m_Position(Position),
+		m_UPic(UPic),
+		m_VPic(VPic),
+		m_Texture(Texture)
+	{}
 
+	Floor::~Floor()
+	{
+	}
+	void Floor::OnCreate() {
+		auto PtrTransform = GetComponent<Transform>();
+
+		PtrTransform->SetScale(m_Scale);
+		PtrTransform->SetRotation(m_Rotation);
+		PtrTransform->SetPosition(m_Position);
+
+		auto ptrColl = AddComponent<CollisionObb>();
+		ptrColl->SetFixed(true);
+		vector<VertexPositionNormalTexture> vertices;
+		vector<uint16_t> indices;
+		MeshUtill::CreateCube(1.0f, vertices, indices);
+		float UCount = m_Scale.x / m_UPic;
+		float VCount = m_Scale.z / m_VPic;
+		for (size_t i = 0; i < vertices.size(); i++) {
+			if (vertices[i].textureCoordinate.x >= 1.0f) {
+				vertices[i].textureCoordinate.x = UCount;
+			}
+			if (vertices[i].textureCoordinate.y >= 1.0f) {
+				float FrontBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, 1, 0));
+				float BackBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, -1, 0));
+				if (FrontBetween < 0.01f || BackBetween < 0.01f) {
+					vertices[i].textureCoordinate.y = VCount;
+				}
+			}
+		}
+
+		auto PtrDraw = AddComponent<BcPNTStaticDraw>();
+		PtrDraw->CreateOriginalMesh(vertices, indices);
+		PtrDraw->SetOriginalMeshUse(true);
+		PtrDraw->SetFogEnabled(true);
+		//é©ï™Ç…âeÇ™âfÇËÇ±ÇﬁÇÊÇ§Ç…Ç∑ÇÈ
+		PtrDraw->SetOwnShadowActive(true);
+		PtrDraw->SetMeshResource(L"DEFAULT_CUBE");
+		PtrDraw->SetTextureResource(m_Texture);
+		PtrDraw->SetSamplerState(SamplerState::LinearWrap);
+		SetAlphaActive(true);
+	}
+
+	//-----------------------------------------------------------------
+	//  ï«
+	//-----------------------------------------------------------------
+	Wall::Wall(const shared_ptr<Stage>& StagePtr,
+		const Vec3& Scale,
+		const Vec3& Rotation,
+		const Vec3& Position,
+		float UPic,
+		float VPic,
+		const wstring& Texture
+	) :
+		GameObject(StagePtr),
+		m_Scale(Scale),
+		m_Rotation(Rotation),
+		m_Position(Position),
+		m_UPic(UPic),
+		m_VPic(VPic),
+		m_Texture(Texture)
+	{}
+
+	Wall::~Wall() {
+	}
+
+	void Wall::OnCreate() {
+		auto PtrTransform = GetComponent<Transform>();
+
+		PtrTransform->SetScale(m_Scale);
+		PtrTransform->SetRotation(m_Rotation);
+		PtrTransform->SetPosition(m_Position);
+
+		auto ptrColl = AddComponent<CollisionObb>();
+		ptrColl->SetFixed(true);
+		vector<VertexPositionNormalTexture> vertices;
+		vector<uint16_t> indices;
+		MeshUtill::CreateCube(1.0f, vertices, indices);
+		float UCount = m_Scale.x / m_UPic;
+		float VCount = m_Scale.z / m_VPic;
+		for (size_t i = 0; i < vertices.size(); i++) {
+			if (vertices[i].textureCoordinate.x >= 1.0f) {
+				vertices[i].textureCoordinate.x = UCount;
+			}
+			if (vertices[i].textureCoordinate.y >= 1.0f) {
+				float FrontBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, 1, 0));
+				float BackBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, -1, 0));
+				if (FrontBetween < 0.01f || BackBetween < 0.01f) {
+					vertices[i].textureCoordinate.y = VCount;
+				}
+			}
+		}
+
+		auto PtrDraw = AddComponent<BcPNTStaticDraw>();
+		PtrDraw->CreateOriginalMesh(vertices, indices);
+		//é©ï™Ç…âeÇ™âfÇËÇ±ÇﬁÇÊÇ§Ç…Ç∑ÇÈ
+		PtrDraw->SetOwnShadowActive(true);
+		PtrDraw->SetOriginalMeshUse(true);
+		PtrDraw->SetTextureResource(m_Texture);
+		PtrDraw->SetSamplerState(SamplerState::LinearWrap);
+		//SetAlphaActive(true);
+	}
 
 }
 //end basecross
