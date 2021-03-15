55ab415c06971dcb7575db2995402575871e85e8
Yotomo2000 <2000yotomo@gmail.com>
2021-03-15 10:46:46 +0900

    敵1のモデルの追加と音の鳴る装置も3Dモデルを追加

--- FullSample100/GameSources/Enemy.h
+++ FullSample100/GameSources/Enemy.h
@@ -3,6 +3,28 @@
 
 namespace basecross {
 
+	//“G‚P
+	class Enemy1 : public GameObject
+	{
+	private:
+		Vec3 m_Scale;
+		Vec3 m_Rotation;
+		Vec3 m_Position;
+		float m_UPic;
+		float m_VPic;
+		wstring m_Texture;
+		wstring m_Mesh;
 
+	public:
+		Enemy1(const shared_ptr<Stage>& StagePtr,
+			const Vec3& Scale,
+			const Vec3& Rotation,
+			const Vec3& Position
+		);
+		virtual ~Enemy1();
+		virtual void OnCreate()override;
+		//void OnUpdate();
+
+	};
 }
 //end basecross
