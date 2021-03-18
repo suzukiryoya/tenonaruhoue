f867e3ab87707402eec839de41dc645a1bc92dfe
Yotomo2000 <2000yotomo@gmail.com>
2021-03-18 10:56:37 +0900

    主人公のサイズ変更

--- FullSample100/GameSources/GameStage.cpp
+++ FullSample100/GameSources/GameStage.cpp
@@ -12,7 +12,7 @@ namespace basecross {
 	//	ゲームステージクラス実体
 	//--------------------------------------------------------------------------------------
 	void GameStage::CreateViewLight() {
-		const Vec3 eye(0.0f, 50.0f, -10.0f);
+		const Vec3 eye(0.0f, 10.0f, -10.0f);
 		const Vec3 at(0.0f);
 		auto PtrView = CreateView<SingleView>();
 		//ビューのカメラの設定
@@ -92,7 +92,7 @@ namespace basecross {
 
 	void GameStage::CreatePlayer()
 	{
-		AddGameObject<Player>(Vec3(1), Vec3(0), Vec3(1.0f,1.0f,0));
+		AddGameObject<Player>(Vec3(1.0f), Vec3(0), Vec3(1.0f,1.0f,-0.5f));
 	}
 
 	void GameStage::CreateCellMap() {
