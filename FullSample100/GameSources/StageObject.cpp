780ef2c77467ff4c8c779990382418c180b1e58c
tsilm08 <takanori.yoshinori@outlook.jp>
2021-04-20 11:35:27 +0900

    スタート地点用のクラス追加、カラムにStartを追加

--- FullSample100/GameSources/StageObject.cpp
+++ FullSample100/GameSources/StageObject.cpp
@@ -265,5 +265,76 @@ namespace basecross{
 		PtrDraw->SetSamplerState(SamplerState::LinearWrap);
 		SetAlphaActive(true);
 	}
+	//-----------------------------------------------------------------
+	//  ステージ　ゴール
+	//-----------------------------------------------------------------
+	Start::Start(const shared_ptr<Stage>& StagePtr,
+		const Vec3& Scale,
+		const Vec3& Rotation,
+		const Vec3& Position,
+		float UPic,
+		float VPic
+
+	) :
+		GameObject(StagePtr),
+		m_Scale(Scale),
+		m_Rotation(Rotation),
+		m_Position(Position),
+		m_UPic(UPic),
+		m_VPic(VPic),
+		m_Texture(L"ステージ床.jpg"),
+		m_Mesh(L"Stairs.bmf")
+	{}
+
+	Start::~Start() {
+	}
+
+	void Start::OnCreate() {
+		auto PtrTransform = GetComponent<Transform>();
+
+		PtrTransform->SetScale(m_Scale);
+		PtrTransform->SetRotation(m_Rotation);
+		PtrTransform->SetPosition(m_Position);
+
+		auto ptrColl = AddComponent<CollisionObb>();
+		ptrColl->SetFixed(true);
+
+		vector<VertexPositionNormalTexture> vertices;
+		vector<uint16_t> indices;
+		MeshUtill::CreateCube(1.0f, vertices, indices);
+		float UCount = m_Scale.x / m_UPic;
+		float VCount = m_Scale.z / m_VPic;
+		for (size_t i = 0; i < vertices.size(); i++) {
+			if (vertices[i].textureCoordinate.x >= 1.0f) {
+				vertices[i].textureCoordinate.x = UCount;
+			}
+			if (vertices[i].textureCoordinate.y >= 1.0f) {
+				float FrontBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, 1, 0));
+				float BackBetween = bsm::angleBetweenNormals(vertices[i].normal, Vec3(0, -1, 0));
+				if (FrontBetween < 0.01f || BackBetween < 0.01f) {
+					vertices[i].textureCoordinate.y = VCount;
+				}
+			}
+		}
+
+		Mat4x4 SpanMat;
+		SpanMat.affineTransformation(
+			Vec3(1.0f, 1.0f, 1.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f));
+
+		auto ShadowPtr = AddComponent<Shadowmap>();
+		ShadowPtr->SetMeshResource(m_Mesh);
+		ShadowPtr->SetMeshToTransformMatrix(SpanMat);
+
+		auto PtrDraw = AddComponent<BcPNTStaticDraw>();
+		PtrDraw->CreateOriginalMesh(vertices, indices);
+		PtrDraw->SetTextureResource(m_Texture);
+		PtrDraw->SetMeshResource(m_Mesh);
+		PtrDraw->SetSamplerState(SamplerState::LinearWrap);
+		SetAlphaActive(true);
+	}
+
 }
 //end basecross
