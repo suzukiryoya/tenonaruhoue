39675511007517699efd19a52db18fd5a7932983
tsilm08 <takanori.yoshinori@outlook.jp>
2021-05-06 15:22:10 +0900

    セレクト実行確認

--- FullSample100/GameSources/SelectStage.h
+++ FullSample100/GameSources/SelectStage.h
@@ -1,21 +1,22 @@
-/*!
-@file GameStage.h
-@brief ゲームステージ
-*/
-
 #pragma once
 #include "stdafx.h"
 
 namespace basecross {
 
 	//--------------------------------------------------------------------------------------
-	//	ゲームステージクラス
+	//	セレクトステージ
 	//--------------------------------------------------------------------------------------
 	class SelectStage : public Stage {
 		//ビューの作成
 		void CreateViewLight();
 		void CreateBGM();
 		void CreateUI();
+
+		void ChangeSelect(int num);
+		vector<shared_ptr<Select_UI>> m_Select;
+		int m_StageNum = 0;
+		bool m_ControlLock;
+
 		wstring m_title_image;
 	public:
 		//構築と破棄
@@ -25,7 +26,10 @@ namespace basecross {
 		virtual ~SelectStage() {}
 		//初期化
 		virtual void OnCreate()override;
-		virtual void OnUpdate() override;
+		virtual void OnUpdate()override;
+
+		int GetStageNum() { return m_StageNum; }
+		vector<shared_ptr<Select_UI>> GetSelectStage() const { return m_Select; }
 	};
 
 
